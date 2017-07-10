c9e1ac872cc2f21a32092e4f9a20bcf4e4a9f00f
tomibolo <tomi_bolo@hotmail.com>
2017-07-10 10:18:56 -0300

    Fiest Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# 94_Edufy_Angular
+# yeoman
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.16.0.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanApp
+ * @description
+ * # yeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix(''); //Arreglo el codigo de la URL por la version de Angular
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
