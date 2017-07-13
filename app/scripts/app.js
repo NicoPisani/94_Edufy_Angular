6b17dcc412cbd4186dad7355cb3420caac10889d
FeR.Catalano <ferc_vcp@hotmail.com>
2017-07-13 09:32:08 -0300

    Cambios en las URL

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,47 +32,47 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/detalle-curso', {
+      .when('/detalle/curso', {
         templateUrl: 'views/detalle-curso.html',
         controller: 'DetalleCursoCtrl',
         controllerAs: 'detalleCurso'
       })//------------------------------------------------------------
-      .when('/panel/user-perfil', {
+      .when('/panel/user/perfil', {
         templateUrl: 'views/users/user-perfil.html',
         controller: 'UserPerfilCtrl',
         controllerAs: 'UserPerfil'
       })
-      .when('/panel/user-favoritos', {
+      .when('/panel/user/favoritos', {
         templateUrl: 'views/users/user-favoritos.html',
         controller: 'UserFavoritosCtrl',
         controllerAs: 'UserFavoritos'
       })
-      .when('/panel/user-pagos', {
+      .when('/panel/user/pagos', {
         templateUrl: 'views/users/user-pagos.html',
         controller: 'UserPagosCtrl',
         controllerAs: 'UserPagos'
       })
-      .when('/panel/user-cursos', {
+      .when('/panel/user/cursos', {
         templateUrl: 'views/users/user-cursos.html',
         controller: 'UserCursosCtrl',
         controllerAs: 'UserCursos'
       })//------------------------------------------------------------
-       .when('/panel/profe-cursos', {
+       .when('/panel/profe/cursos', {
         templateUrl: 'views/profes/profe-cursos.html',
         controller: 'ProfeCursosCtrl',
         controllerAs: 'ProfeCursos'
       })
-       .when('/panel/profe-perfil', {
+       .when('/panel/profe/perfil', {
         templateUrl: 'views/profes/profe-perfil.html',
         controller: 'ProfePerfilCtrl',
         controllerAs: 'ProfePerfil'
       })
-       .when('/panel/profe-nuevo', {
+       .when('/panel/profe/nuevo', {
         templateUrl: 'views/profes/profe-nuevo.html',
         controller: 'ProfeNuevoCtrl',
         controllerAs: 'ProfeNuevo'
       })
-       .when('/panel/profe-archivos', {
+       .when('/panel/profe/archivos', {
         templateUrl: 'views/profes/profe-archivos.html',
         controller: 'ProfeArchivosCtrl',
         controllerAs: 'ProfeArchivos'
