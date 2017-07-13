30c654b44cdd6d4dd6817cd9c63825a0b72e1d5b
NicoPisani <hi@nicolaspisani.com.ar>
2017-07-13 10:20:56 -0300

    Modificacion URL

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -37,42 +37,42 @@ angular
         controller: 'DetalleCursoCtrl',
         controllerAs: 'detalleCurso'
       })//------------------------------------------------------------
-      .when('/panel/user-perfil', {
+      .when('/panel/user/perfil', {
         templateUrl: 'views/users/user-perfil.html',
         controller: 'UserPerfilCtrl',
         controllerAs: 'UserPerfil'
       })
-      .when('/panel/user-favoritos', {
+      .when('/panel/user/favoritos', {
         templateUrl: 'views/users/user-favoritos.html',
         controller: 'UserFavoritosCtrl',
         controllerAs: 'UserFavoritos'
       })
-      .when('/panel/user-pagos', {
+      .when('/panel/user/pagos', {
         templateUrl: 'views/users/user-pagos.html',
         controller: 'UserPagosCtrl',
         controllerAs: 'UserPagos'
       })
-      .when('/panel/user-cursos', {
+      .when('/panel/user/cursos', {
         templateUrl: 'views/users/user-cursos.html',
         controller: 'UserCursosCtrl',
         controllerAs: 'UserCursos'
       })//------------------------------------------------------------
-       .when('/panel/profe-cursos', {
+       .when('/panel/profesor/cursos', {
         templateUrl: 'views/profes/profe-cursos.html',
         controller: 'ProfeCursosCtrl',
         controllerAs: 'ProfeCursos'
       })
-       .when('/panel/profe-perfil', {
+       .when('/panel/profesor/perfil', {
         templateUrl: 'views/profes/profe-perfil.html',
         controller: 'ProfePerfilCtrl',
         controllerAs: 'ProfePerfil'
       })
-       .when('/panel/profe-nuevo', {
+       .when('/panel/profesor/nuevo', {
         templateUrl: 'views/profes/profe-nuevo.html',
         controller: 'ProfeNuevoCtrl',
         controllerAs: 'ProfeNuevo'
       })
-       .when('/panel/profe-archivos', {
+       .when('/panel/profesor/archivos', {
         templateUrl: 'views/profes/profe-archivos.html',
         controller: 'ProfeArchivosCtrl',
         controllerAs: 'ProfeArchivos'
