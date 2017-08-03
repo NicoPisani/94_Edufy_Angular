508e69a07f731548ced2c45be446b6a5bd5f0186
FeR.Catalano <ferc_vcp@hotmail.com>
2017-08-03 10:39:58 -0300

    registro usuario

--- app/scripts/controllers/detalleCursoController.js
+++ app/scripts/controllers/detalleCursoController.js
@@ -8,8 +8,10 @@
  * Controller of the yeomanApp
  */
 angular.module('yeomanApp')
-  .controller('DetalleCursoCtrl', function ($scope, $resource, $routeParams) {
+  .controller('DetalleCursoCtrl', function (GLOBAL, $http, $scope, $resource, $routeParams ) {
+
     var nv = this;
+
     nv.menuTemplate = {
       url : 'views/navbar/navbar.html'
     },
@@ -17,76 +19,89 @@ angular.module('yeomanApp')
       url : 'views/footer/footer.html'
     }
 
-    nv.Curso = $resource("http://localhost:8000/api/curso/:id", {id: "@id"});
-    $scope.curso = nv.Curso.get({id: $routeParams.id}); // devuelve 1 ob JSON / isArray = false 
+	   //trae datos del curso
+     //nv.Curso = $resource(GLOBAL.URL_API+"curso/:id", {id: "@id"});
+     //$scope.curso = nv.Curso.get({id: $routeParams.id})
+/*------------------------------------------------------------*/
 
-  })
 
-  .controller('profesorCtrl', function ($scope, $resource) {
-	    var nv = this;
+    $http.get(GLOBAL.URL_API+"curso/"+$routeParams.id)    
+    .then(
+     function (response) {
+       $scope.curso = response.data;
+        nv.User = $resource(GLOBAL.URL_API+"user-public/:id", {id: "@id"});
+        $scope.user = nv.User.get({ id: $scope.curso.user_id });
+     },
+     function (error) {
+       if(error.data.error === 'token_not_provided') {
+         console.log('error')
+       }
+     });
 
-	   nv.User = $resource("http://localhost:8000/api/user-public/:id", {id: "@id"});
-	   $scope.user = nv.User.get({id: '3'});
 
-	   console.log($scope.curso.user_id);
+    $scope.comprar = function(_curso){
+      alert("Gracias por comprar el curso -> "+_curso)
+    }
+	
   })
 
   .controller('tabController', function ($scope) {
 
-        $scope.IsVisible = true;
-        $scope.ShowHide = function () {
-            $scope.IsVisible = $scope.IsVisible ? false : true;
-        };
+	// modulos
+    $scope.IsVisible = true;
+    $scope.ShowHide = function () {
+        $scope.IsVisible = $scope.IsVisible ? false : true;
+    };
 
-	    //Array de Modulos
-	    $scope.modulos = [
-	      {
-	        'titulo' : 'MÓDULO 1 - PRÁCTICAS DE LENGUAJE',
-	        'descripcion' : "Let's take a look at the areas you should consider for security. Attack surfaces, data transmission and storage, and more.",
-	        'visible' : true,
-	        clases : [
-	           {
-	            'titulo' : 'CLASE 1: INTRODUCCIÓN A LA ORATORIA',
-	            'visible' : true
-	          },
-	          {
-	            'titulo' : 'CLASE 2: INTRODUCCIÓN A LA ORATORIA',
-	            'visible' : true
-	          }
+    //Array de Modulos
+    $scope.modulos = [
+      {
+        'titulo' : 'MÓDULO 1 - PRÁCTICAS DE LENGUAJE',
+        'descripcion' : "Let's take a look at the areas you should consider for security. Attack surfaces, data transmission and storage, and more.",
+        'visible' : true,
+        clases : [
+           {
+            'titulo' : 'CLASE 1: INTRODUCCIÓN A LA ORATORIA',
+            'visible' : true
+          },
+          {
+            'titulo' : 'CLASE 2: INTRODUCCIÓN A LA ORATORIA',
+            'visible' : true
+          }
 
-	        ]
-	      },
-	      {
-	        'titulo' : 'MÓDULO 2 - PRÁCTICAS DE LENGUAJE',
-	        'descripcion' : "Let's take a look at the areas you should consider for security. Attack surfaces, data transmission and storage, and more.",
-	        'visible' : true,
-	        clases : [
-	           {
-	            'titulo' : 'CLASE 1: INTRODUCCIÓN A LA ORATORIA',
-	            'visible' : true
-	          },
-	          {
-	            'titulo' : 'CLASE 2: INTRODUCCIÓN A LA ORATORIA',
-	            'visible' : true
-	          }
+        ]
+      },
+      {
+        'titulo' : 'MÓDULO 2 - PRÁCTICAS DE LENGUAJE',
+        'descripcion' : "Let's take a look at the areas you should consider for security. Attack surfaces, data transmission and storage, and more.",
+        'visible' : true,
+        clases : [
+           {
+            'titulo' : 'CLASE 1: INTRODUCCIÓN A LA ORATORIA',
+            'visible' : true
+          },
+          {
+            'titulo' : 'CLASE 2: INTRODUCCIÓN A LA ORATORIA',
+            'visible' : true
+          }
 
-	        ]
-	      },
-	      {
-	        'titulo' : 'MÓDULO 3 - PRÁCTICAS DE LENGUAJE',
-	        'descripcion' : "Let's take a look at the areas you should consider for security. Attack surfaces, data transmission and storage, and more.",
-	        'visible' : true,
-	        clases : [
-	           {
-	            'titulo' : 'CLASE 1: INTRODUCCIÓN A LA ORATORIA',
-	            'visible' : true
-	          },
-	           {
-	            'titulo' : 'CLASE 2: INTRODUCCIÓN A LA ORATORIA',
-	            'visible' : true
-	          }
+        ]
+      },
+      {
+        'titulo' : 'MÓDULO 3 - PRÁCTICAS DE LENGUAJE',
+        'descripcion' : "Let's take a look at the areas you should consider for security. Attack surfaces, data transmission and storage, and more.",
+        'visible' : true,
+        clases : [
+           {
+            'titulo' : 'CLASE 1: INTRODUCCIÓN A LA ORATORIA',
+            'visible' : true
+          },
+           {
+            'titulo' : 'CLASE 2: INTRODUCCIÓN A LA ORATORIA',
+            'visible' : true
+          }
 
-	        ]
-	      }
-	    ];
+        ]
+      }
+    ];
    });
--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -8,7 +8,7 @@
  * Controller of the yeomanApp
  */
 angular.module('yeomanApp')
-  .controller('MainCtrl', function (GLOBAL, $http, $scope, $resource, $routeParams) {
+  .controller('MainCtrl', function (GLOBAL, $http, $scope, $routeParams) {
     
     $scope.global = GLOBAL;
 
@@ -41,4 +41,39 @@ angular.module('yeomanApp')
        }
      });
 
-  });
+  })
+
+  .controller('Signup', function (GLOBAL, authUser, sessionControl, $http, $scope, $location, toastr){
+
+    var sn = this;
+
+    var C = authUser.isLoggedIn();
+    $scope.IsAuthenticated = (C||null)==null?false:true;
+
+
+    sn.registerForm = {
+      name: '',
+      email: '',
+      password: '',
+      rol: 2,
+      active: 1,
+      plataforma_id: 1
+    };
+
+    sn.registrar = function (){
+      $http({
+        url: GLOBAL.URL_API+"auth/signup",
+        method: "POST",
+        data:  sn.registerForm,
+        /*headers: {'Content-Type': 'application/x-www-form-urlencoded'}*/
+      }).then(
+        function (respuesta){
+          toastr.success('Registro completado conrrectamente!', 'Mensaje');
+          $location.path('/login');
+        },
+        function (error) {
+           toastr.error('Algo salio mal, vuelve a intentarlo', 'Mensaje');
+       });
+    }
+
+  })
